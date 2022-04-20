f3d6d075dd96a7d2f8942d2c54b6d60566a1353c
Mark McDermott <mark@markmcdermott.io>
2022-04-20 19:17:44 +0000

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root 'home#index'
+  
+  get '/about', to: "static_pages#about"
+  get '/contact', to: "static_pages#contact"
+  get '/', :to => "home#index", :as => :home
+  
 end
